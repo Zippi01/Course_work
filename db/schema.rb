959682ff23d6067d67c03386e90bbb1913abea8d
Zippi01 <katyakovtun080619@gmail.com>
2019-12-23 16:56:40 +0200

    pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_12_09_185927) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cities", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
