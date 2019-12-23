a8bf075da93a0012212b5bc10ccd110a32e25666
Zippi01 <katyakovtun080619@gmail.com>
2019-12-23 17:55:37 +0200

    registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_09_185927) do
+ActiveRecord::Schema.define(version: 2019_12_23_150028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2019_12_09_185927) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "city_id"
     t.integer "institution_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
